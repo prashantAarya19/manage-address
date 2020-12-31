646b6fcf2bf682d54fe732137d77c1a8aec33265
Prashant Kumar <prashant.kumar@htdigital.in>
2020-12-31 17:25:25 +0530

    first commit --> project setup

---
+++ manage-address/src/main/java/com/manageaddress/ManageAddressApplication.java
@@ -0,0 +1,13 @@
+package com.manageaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageAddressApplication.class, args);
+	}
+
+}
---
+++ manage-address/src/test/java/com/manageaddress/ManageAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manageaddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
